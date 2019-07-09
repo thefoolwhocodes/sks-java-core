682a0c8e2c03065d6de008793874d669e187c21d
Shashi Kant <shashikantsantosh@gmail.com>
2019-07-09 14:09:07 +0530

    ExecutorService - ThreadPoolExecutor demonstration

---
+++ javacore/src/main/java/com/sks/javacore/ExecutorService/ThreadPoolExecutorDemo.java
@@ -0,0 +1,75 @@
+package com.sks.javacore.ExecutorService;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+
+/*
+ * The java.util.concurrent.ThreadPoolExecutor is an implementation of the ExecutorService interface.
+ * The ThreadPoolExecutor executes the given task (Callable or Runnable) using one of its internally pooled threads.
+ */
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+public class ThreadPoolExecutorDemo {
+    private ThreadPoolExecutor INDEX_EXECUTOR;
+    private Thread workProducer;
+    private static final int workCount = 20;
+
+    public ThreadPoolExecutorDemo() {
+        int corePoolSize = 5;
+        int maxPoolSize = workCount;
+        long keepAliveTime = 5000;
+        INDEX_EXECUTOR = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(maxPoolSize),
+                new ThreadFactoryBuilder().setNameFormat("IndexExecutor-%d").setDaemon(true).build());
+        INDEX_EXECUTOR.prestartAllCoreThreads();
+        workProducer = new Thread(new WorkProducer());
+    }
+
+    public void startDemo() {
+        workProducer.start();
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutorDemo demo = new ThreadPoolExecutorDemo();
+        demo.startDemo();
+    }
+
+    class WorkProducer implements Runnable {
+        public void run() {
+            while (true) {
+                System.out.println("Going to produce %d works" + workCount);
+                for (int i = 0; i < workCount; i++) {
+                    try {
+                        INDEX_EXECUTOR.submit(new Tasks(i));
+                    } catch (RejectedExecutionException e) {
+                        // Exception could arise when the Executor cannot take any more tasks under its responsibility.
+                        // If we try to submit more tasks than the ArrayBlockingQueue can hold, then 
+                        // RejectedExecutionException arises.
+                        System.out.println("WorkProducer - RejectedExecutionException" + e);
+                    }
+                }
+                try {
+                    System.out.println("Going to sleep for 5 seconds");
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("WorkProducer - InterruptedException occurred");
+                }
+            }
+        }
+    }
+
+    class Tasks implements Runnable {
+        private int _taskId;
+
+        public Tasks(int taskId) {
+            _taskId = taskId;
+        }
+        public void run() {
+            System.out.println("Started running task " + _taskId);
+            System.out.println("Completed running task " + _taskId);
+        }
+    }
+}
