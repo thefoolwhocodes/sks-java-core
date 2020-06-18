6663b19ba3c264476ee093085930f67faecb15dd
Shashi Kant <shashikantsantosh@gmail.com>
2020-06-18 22:42:06 +0530

    Demonstration for BiFunction usage in Java HashMap

--- javacore/src/main/java/com/sks/javacore/collections/hashmap/HashMapDemonstrator.java
+++ javacore/src/main/java/com/sks/javacore/collections/hashmap/HashMapDemonstrator.java
@@ -3,7 +3,10 @@
  */
 package com.sks.javacore.collections.hashmap;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.function.BiFunction;
 
 public class HashMapDemonstrator {
 
@@ -30,8 +33,58 @@ public class HashMapDemonstrator {
         System.out.println("Finished - replaceAll");
     }
 
+    private static void replaceAllByFilter() {
+        System.out.println("Started - replaceAllByFilter");
+
+        Collection<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(5);
+
+        {
+            System.out.println("Started - replaceAllByFilter - example 1");
+            HashMap<Integer, Integer> objHM = new HashMap<Integer, Integer>();
+            objHM.put(1, 10);
+            objHM.put(2, 20);
+            objHM.put(3, 30);
+            objHM.put(4, 40);
+            objHM.put(5, 50);
+
+            BiFunction<Integer, Integer, Integer> function1 = (s1, s2) -> {
+                if (list.contains(s1))
+                    return s2 + 1;
+                else
+                    return s2;
+            };
+
+            objHM.replaceAll(function1);
+            System.out.println(objHM);
+        }
+
+        {
+            System.out.println("Started - replaceAllByFilter - example 2");
+            HashMap<Integer, Integer> objHM = new HashMap<Integer, Integer>();
+            objHM.put(1, 10);
+            objHM.put(2, 20);
+            objHM.put(3, 30);
+            objHM.put(4, 40);
+            objHM.put(5, 50);
+
+            objHM.replaceAll((s1, s2) -> {
+                if (list.contains(s1))
+                    return s2 + 1;
+                else
+                    return s2;
+            });
+
+            System.out.println(objHM);
+        }
+
+        System.out.println("Finished - replaceAllByFilter");
+    }
+
     public static void main(String[] args) {
         put();
         replaceAll();
+        replaceAllByFilter();
     }
 }
