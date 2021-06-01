9e6a88d2baf3560ae487dc8e12be2495a5496e1e
Shashi Kant <shashikantsantosh@gmail.com>
2021-06-01 23:25:13 +0530

    Adding more examples for ArrayList

--- javacore/src/main/java/com/sks/javacore/collections/list/ArrayListDemonstrator.java
+++ javacore/src/main/java/com/sks/javacore/collections/list/ArrayListDemonstrator.java
@@ -7,14 +7,18 @@
 package com.sks.javacore.collections.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListDemonstrator {
 
-    private static void demonstrateArrayList()
+    /**
+     * Demonstration for ways to iterate over the list
+     */
+    private static void demonstrateArrayList1()
     {
-        System.out.println("Started - demonstrateArrayList");
+        System.out.println("Started - demonstrateArrayList1");
 
         List<String> list = new ArrayList<String>(4);
         list.add("Amazon");
@@ -43,11 +47,29 @@ public class ArrayListDemonstrator {
             System.out.println("stream forEach's Element: " + elements);
         });
 
-        System.out.println("Finished - demonstrateArrayList");
+        System.out.println("Finished - demonstrateArrayList1");
     }
 
+    public static void printList(List<String> accounts) {
+        for (String item : accounts) {
+            System.out.println(item);
+        }
+    }
+
+    /**
+     * Demonstration to initialize list in one line
+     */
+    public static void demonstrateArrayList2() {
+        System.out.println("Started - demonstrateArrayList2");
+
+        printList(Arrays.asList(new String[] { "data1", "data2" }));
+
+        System.out.println("Finished - demonstrateArrayList2");
+    }
+    
     public static void main(String[] args) {
-        demonstrateArrayList();
+        demonstrateArrayList1();
+        demonstrateArrayList2();
     }
 
 }
